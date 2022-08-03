da301bc52d2ac08438e17108af724ba193e8ba69
superflyman90 <41185067+superflyman90@users.noreply.github.com>
2022-08-03 20:51:37 +0200

    Initial commit

---
+++ src/components/Articles.js
@@ -0,0 +1,9 @@
+const Articles = () => {
+  return (
+    <>
+      <h3>Listes des Articles</h3>
+    </>
+  );
+};
+
+export default Articles;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import Articles from "../components/Articles";
+
+const Home = () => {
+  return (
+    <>
+      <h2>Hello CodeSandbox</h2>
+      <Articles />
+    </>
+  );
+};
+
+export default Home;
